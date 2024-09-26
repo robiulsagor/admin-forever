e9195c43a49164bed88fcfc7cb8710756042d642
robiulsagor <robiul100.me@gmail.com>
2024-09-26 21:50:13 +0600

    Product listing function with deletion done

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,6 +1,88 @@
-const List = () => {
+import { RiDeleteBin6Line } from "react-icons/ri";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+import Loading from "../components/Loading";
+import { useEffect, useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+const List = ({ token }) => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  console.log(products);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true)
+        const res = await axios.get(backendUrl + '/product/list', { headers: { token } })
+        setProducts(res.data.products)
+      } catch (error) {
+        console.log(error)
+        toast.error(error.response.data.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  const deleteProduct = async (id) => {
+    const confirm = window.confirm("Are you sure you want to delete this product?")
+    if (!confirm) return
+
+    try {
+      const res = await axios.post(backendUrl + '/product/delete', { productId: id }, { headers: { token } })
+      if (res.data.success) {
+        toast.success("Product deleted successfully!")
+        setProducts(products.filter(product => product._id !== id))
+      } else {
+        toast.error(res.data.message)
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message ? error.response.data.message : "Something went wrong")
+    }
+  }
+
   return (
-    <div>List</div>
+    <div className="w-full pt-4 pb-10 ">
+      <h2 className="text-xl font-medium text-gray-600 mb-6">All Products List</h2>
+      {loading && <div className="flex justify-center items-center"><Loading /></div>}
+
+      {!loading && products.length === 0 && <div className="flex justify-center items-center"><p className="text-gray-500">No products found</p></div>}
+
+      {!loading && products.length > 0 && (
+        <>
+          <div className="bg-slate-100 grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_0.5fr] text-sm p-1 font-medium text-gray-700">
+            <p>Image</p>
+            <p>Name</p>
+            <p className="hidden md:block">Category</p>
+            <p className="hidden md:block">Price</p>
+            <p>Action</p>
+          </div>
+
+          <div className="mt-2 ">
+            {products.map((product, index) => (
+              <div key={index} className="mb-2 border rounded p-1 grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_0.5fr] text-sm items-center text-gray-700">
+                <div className="border w-14 h-14 object-cover rounded">
+                  <img src={product.image[0]} className="w-full h-full object-cover" alt="" />
+                </div>
+                <p>{product.name}</p>
+                <p className="hidden md:block">{product.category}</p>
+                <p className="hidden md:block">${product.price}</p>
+                <div className="flex gap-2">
+                  <button onClick={() => deleteProduct(product._id)} className="bg-red-500 text-white px-2 py-1 rounded-md">
+                    <RiDeleteBin6Line />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,21 @@
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className="w-full pt-4 pb-10">
+      <h2 className="text-xl font-medium text-gray-700 mb-3">All Orders</h2>
+
+      <div>
+        {Array.from({ length: 2 }).map((_, index) => (
+          <div key={index} className="border border-gray-300 rounded p-2 mb-2 grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr]">
+            <img src="https://raw.githubusercontent.com/avinashdm/gs-images/main/forever/p_img47.png" className="w-12 h-12 object-cover rounded" alt="" />
+
+            <div>
+              <h3 className="text-lg font-medium text-gray-700">Product Name</h3>
+            </div>
+          </div>
+
+        ))}
+      </div>
+    </div>
   )
 }
 
